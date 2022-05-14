18600fdb423779ea6234b9b36db1cf29dbae8445
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-05-14 14:48:02 +0100

    Server

--- server.js
+++ server.js
@@ -1,4 +1,39 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
 
-module.exports = sequelize;
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const createSession = {
+  secret: 'Lemon',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(createSession));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Application now listening on http://localhost:${PORT}`));
+});
