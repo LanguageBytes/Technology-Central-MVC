9f3445036d7f56046fb2a3268737d24c953003af
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-05-17 21:33:19 +0100

    heroku

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -8,7 +7,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3002;
 
 const createSession = {
   secret: 'Lemon',
