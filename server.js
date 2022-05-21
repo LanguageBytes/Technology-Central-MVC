cf1f05b4ec2dd48d42c4583702cf194e83a84f10
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-05-21 15:50:36 +0100

    testing heroku fixes

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Application now listening on http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
 });
